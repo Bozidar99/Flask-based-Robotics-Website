9cda4838d9becd5ae5e66f000951a31534831f21
Bozidar Bajovic <99490511+Bozidar99@users.noreply.github.com>
2024-03-27 22:43:36 +0000

    Zavrsen program

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+
+db =SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.String)
+    text = db.Column(db.String)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -16,7 +30,32 @@ def contact():
 def news():
     return render_template("about.html")
 
-@app.route("/team")
-def article():
-    return render_template("team.html")
+@app.route("/blog", methods=["GET"])
+def blog():
 
+    message = Message.query.all()
+
+    return render_template("blog.html", message=message)
+
+@app.route("/add-message", methods=["POST"])
+def add_message():
+
+    username = request.form.get("username")
+    text = request.form.get("text")
+
+    message = Message(username = username, text = text)
+
+    db.session.add(message)
+    db.session.commit()
+
+    return redirect("/blog")
+
+@app.route("/delete/<msg_id>", methods=["GET"])
+def delete_message(msg_id):
+
+    message = Message.query.get(int(msg_id))
+    
+    db.session.delete(message)
+    db.session.commit()
+
+    return redirect("/blog")
